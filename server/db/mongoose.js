edecda013e87f05ab2819522ac475881a81b42c6
malilovick <malilovick@gmail.com>
2018-10-24 16:41:02 -0400

    add queries playground

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
-
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://localhost:27017/TodoApp", {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
+module.exports = { mongoose };
 
-module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,32 +1,49 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require("./db/mongoose");
+var { Todo } = require("./models/todo");
+var { User } = require("./models/user");
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  })
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.get("/users", (req, res) => {
+  User.find().then(
+    users => {
+      res.send({ users });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 app.post("/users", (req, res) => {
@@ -48,7 +65,7 @@ app.post("/users", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log("Started on port 3000");
 });
 
-module.exports = {app};
+module.exports = { app };
