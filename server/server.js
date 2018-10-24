ecbdc602e03525d49e9ec94ecb8ef224362f34a8
malilovick <malilovick@gmail.com>
2018-10-24 15:48:13 -0400

    add test

--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var { mongoose } = require("./db/mongoose");
-var { Todo } = require("./models/todo");
-var { User } = require("./models/user");
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post("/todos", (req, res) => {
-  //  console.log(req.body);
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
 
-  todo.save().then(
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.post("/users", (req, res) => {
+  var user = new User({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user.save().then(
     doc => {
       res.send(doc);
     },
@@ -26,43 +48,7 @@ app.post("/todos", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Started on port 3000");
+  console.log('Started on port 3000');
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo')
-// });
-
-// var otherTodo = new Todo({
-//   text: '             Feed the cat        ',
-//   completed: true,
-//   completedAt: 123
-// });
-
-// otherTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Unable to save', e);
-// });
-
-// var newUser = new User({
-//   name: 'Marcos Viana',
-//   username: 'malilovick',
-//   email: 'malilovick@gmail.com',
-//   password: '123456789@'
-// });
-
-// newUser.save().then(
-//   doc => {
-//     console.log(JSON.stringify(doc, undefined, 2));
-//   },
-//   e => {
-//     console.log("Unable to save", e);
-//   }
-// );
+module.exports = {app};
